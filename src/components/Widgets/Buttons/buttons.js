de399a4b6c7f366736e9865117fe87b904ab074b
montygoldy <montygoldy2006@gmail.com>
2018-03-31 14:14:42 -0400

    Working load more news button with css trannsitions as well as added reusable button widget

---
+++ src/components/Widgets/Buttons/buttons.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./buttons.css";
+
+const Button = props => {
+  let template = null;
+  switch (props.type) {
+    case "loadmore":
+      template = (
+        <div className="blue_btn" onClick={props.loadMore}>
+          {props.text}
+        </div>
+      );
+      break;
+    default:
+      template = null;
+  }
+  return template;
+};
+
+export default Button;
--- src/components/Widgets/NewsList/NewsList.js
+++ src/components/Widgets/NewsList/NewsList.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Link } from "react-router-dom";
 import { URL } from "../../../config";
-
+import "./newsList.css";
+import Button from "../Buttons/buttons";
 class NewsList extends Component {
   state = {
     items: [],
@@ -12,12 +13,64 @@ class NewsList extends Component {
   };
 
   componentWillMount() {
-    fetch(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
-      .then(res => res.json())
-      .then(data => this.setState({ items: [...this.state.items, ...data] }));
+    this.request(this.state.start, this.state.end);
   }
+
+  renderNews = type => {
+    let template = null;
+    switch (type) {
+      case "card":
+        template = this.state.items.map((item, i) => (
+          <CSSTransition
+            classNames={{
+              enter: "newsList_wrapper",
+              enterActive: "newsList_wrapper_enter"
+            }}
+            timeout={500}
+            key={item.id}
+          >
+            <div>
+              <div className="newsList_item">
+                <Link to={`/articles/${item.id}`}>
+                  <h2>{item.title}</h2>
+                </Link>
+              </div>
+            </div>
+          </CSSTransition>
+        ));
+        break;
+      default:
+        template = null;
+    }
+    return template;
+  };
+
+  request = (start, end) => {
+    fetch(`${URL}/articles?_start=${start}&_end=${end}`)
+      .then(res => res.json())
+      .then(data =>
+        this.setState({ items: [...this.state.items, ...data], start, end })
+      );
+  };
+
+  loadMore = () => {
+    let end = this.state.end + this.state.amount;
+    this.request(this.state.end, end);
+  };
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <TransitionGroup component="div" className="list">
+          {this.renderNews(this.props.type)}
+        </TransitionGroup>
+        <Button
+          type="loadmore"
+          loadMore={this.loadMore}
+          text="Load more news"
+        />
+      </div>
+    );
   }
 }
 

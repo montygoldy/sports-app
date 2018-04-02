96bc7d2d45e4b33926986c21ff591b17d918f853
montygoldy <montygoldy2006@gmail.com>
2018-04-01 22:37:14 -0400

    Fixed code issue with videos main component

--- src/components/Articles/videos/videosMain/videoMain.js
+++ src/components/Articles/videos/videosMain/videoMain.js
@@ -4,9 +4,9 @@ import VideoList from "../../../Widgets/VideoList/VideoList";
 const VideosMain = () => {
   return (
     <div>
-      <VideoList type="card" title={false} loadMore={true} start={0} amount={10} />
+      <VideoList type="card" title={false} loadmore={true} start={0} amount={8} />
     </div>
   )
 }
 
-const VideosMain;
+export default VideosMain;
